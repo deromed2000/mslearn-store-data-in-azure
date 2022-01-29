43762afe2aafd39c18cd91ac979e448e1540704b
Mohammed Derouich <deromed2000@gmail.com>
2022-01-29 14:31:46 +0100

    accomplish the java storage webapp

--- store-java-ee-application-data-with-azure-blob-storage/start/src/main/java/com/microsoft/azure/samples/service/BlobStorage.java
+++ store-java-ee-application-data-with-azure-blob-storage/start/src/main/java/com/microsoft/azure/samples/service/BlobStorage.java
@@ -6,24 +6,47 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Singleton;
 
+import java.util.stream.Collectors;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import com.azure.storage.blob.models.BlobItem;
+
 @Singleton
 public class BlobStorage implements Storage {
 
+    private BlobContainerClient blobContainerClient;
+
     @PostConstruct
     private void init() {
-        throw new UnsupportedOperationException();
+        String connectionString = System.getenv("STORAGE_CONNECTION_STRING");
+        String containerName = System.getenv("STORAGE_CONTAINER_NAME");
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+            .connectionString(connectionString)
+            .buildClient();
+        blobContainerClient = blobServiceClient.getBlobContainerClient(containerName);
+        if (!blobContainerClient.exists()) {
+            blobContainerClient.create();
+        }
     }
 
     public List<String> listNames() {
-        throw new UnsupportedOperationException();
+        return blobContainerClient.listBlobs()
+          .stream()
+          .map(BlobItem::getName)
+          .collect(Collectors.toList());
     }
 
     public void save(String name, InputStream inputStream, long contentLength) {
-        throw new UnsupportedOperationException();
+        BlobClient blobClient = blobContainerClient.getBlobClient(name);
+        blobClient.upload(inputStream, contentLength);
     }
 
     public InputStream read(String name) {
-        throw new UnsupportedOperationException();
+        BlobClient blobClient = blobContainerClient.getBlobClient(name);
+        return blobClient.openInputStream();
     }
     
 }
